2909659a79c1dba085b3d9b60bd0389f16a3f628
Gajendran <billgajen@gmail.com>
2017-10-13 09:13:57 +0100

    Debugging base64 image too large issue

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 app.use(morgan('dev'));
 
 // Jade Template Engine
